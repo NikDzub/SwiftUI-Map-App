563f42a02318a39419633ccf4dffbb57e78f2d69
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-11-27 22:22:59 +0200

    .

---
+++ Map/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  Map
+//
+//  Created by ihpt on 27/11/2024.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
