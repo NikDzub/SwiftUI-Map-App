ee90708f6fe1c7d5734cddfcc3781fbbf03c8663
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-11-27 20:05:32 +0200

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by ihpt on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
